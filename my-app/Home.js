540be921e939b543e2399a3ed27001583cb0a1b9
Hu Kaixiang <maxhu787@gmail.com>
2024-12-05 00:43:40 +0800

    update

--- my-app/Home.js
+++ my-app/Home.js
@@ -1,7 +1,126 @@
-import React from "react";
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { IconButton, Text } from "react-native-paper";
+import { StatusBar } from "expo-status-bar";
 
-function Home() {
-  return <div>Home</div>;
+export default function Home() {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <View
+          style={{
+            paddingVertical: 20,
+            width: "100%",
+            alignItems: "center",
+            // backgroundColor: "green",
+          }}
+        >
+          <Text variant="displayMedium">停車費用計算</Text>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "lightblue",
+            width: "100%",
+            padding: 20,
+            gap: 20,
+          }}
+        >
+          <View style={{ flex: 0.1 }}>
+            <Text
+              variant="headlineSmall"
+              style={{ width: 20, backgroundColor: "#fff" }}
+            >
+              每
+            </Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <IconButton
+              style={styles.button}
+              icon="menu-up-outline"
+              iconColor={"#fff"}
+              size={25}
+              onPress={() => console.log("Pressed")}
+            />
+            <TextInput style={styles.input} />
+            <IconButton
+              style={styles.button}
+              icon="menu-down-outline"
+              iconColor={"#fff"}
+              size={25}
+              onPress={() => console.log("Pressed")}
+            />
+          </View>
+          <View style={{ flex: 0.1 }}>
+            <Text
+              variant="headlineSmall"
+              style={{ width: 20, backgroundColor: "#fff" }}
+            >
+              分鐘
+            </Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <IconButton
+              style={styles.button}
+              icon="menu-up-outline"
+              iconColor={"#fff"}
+              size={25}
+              onPress={() => console.log("Pressed")}
+            />
+
+            <TextInput style={styles.input} />
+            <IconButton
+              style={styles.button}
+              icon="menu-down-outline"
+              iconColor={"#fff"}
+              size={25}
+              onPress={() => console.log("Pressed")}
+            />
+          </View>
+          <View style={{ flex: 0.1 }}>
+            <Text
+              variant="headlineSmall"
+              style={{ width: 20, backgroundColor: "#fff" }}
+            >
+              元
+            </Text>
+          </View>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </TouchableWithoutFeedback>
+  );
 }
 
-export default Home;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 40,
+    // alignItems: "center",
+    // justifyContent: "center",
+    color: "#eeefff",
+    // backgroundColor: "red",
+  },
+  button: {
+    backgroundColor: "#000",
+    borderRadius: 12,
+    width: "100%",
+    // margin: 0,
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderRadius: 12,
+    width: "100%",
+    // width: 25,
+    // height: 25,
+    padding: 20,
+    // margin: 10,
+  },
+});
--- my-app/index.js
+++ my-app/index.js
@@ -1,15 +1,8 @@
-import * as React from "react";
-import { AppRegistry } from "react-native";
-import { PaperProvider } from "react-native-paper";
-import { name as appName } from "./app.json";
+import { registerRootComponent } from "expo";
+
 import App from "./App";
 
-export default function Main() {
-  return (
-    <PaperProvider>
-      <App />
-    </PaperProvider>
-  );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
