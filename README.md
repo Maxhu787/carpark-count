97c540a6d1a669f4a7ad16e7affdce872d6ca88e
Hu Kaixiang <maxhu787@gmail.com>
2024-12-06 00:11:26 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # carpark-count
+
+```
+收費
+88$/00 hr
+```
+
+```
+計算規則
+00 hr (以內/之後): 免費/88$/不計算
+不足一小時 (以分鐘計算) or (視為 1 小時) or (不計算)
+```
+
+```
+停車時間 curr time (editable)
+離開世間 input or (max paying price)
+```
+
+```
+其他
+總價×80%
+```
+
+```
+Other features
+(Picture section take pic of spot)
+(Scan payment rules with camera and ai)
+(Keep track of parcking coordinate or car park to remember the rules)
+(Notify me at 00 hrs)
+(Scan feature behind paywall)
+ParkPal ParkPro
+```
--- my-app/Home.js
+++ my-app/Home.js
@@ -5,13 +5,131 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from "react-native";
-import { IconButton, Text } from "react-native-paper";
+import { Modal, Portal, IconButton, Text } from "react-native-paper";
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Dropdown } from "react-native-element-dropdown";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+  const [rate, setRate] = useState("30");
+  const [rateHr, setRateHr] = useState("1");
+
+  const [ruleHr, setRuleHr] = useState("1");
+  const [type, setType] = useState(null);
+  const [calc, setCalc] = useState(null);
+
+  const data1 = [
+    { label: "以內", value: "1" },
+    { label: "之後", value: "2" },
+  ];
+  const data2 = [
+    { label: "免費", value: "3" },
+    { label: "自訂", value: "4" },
+  ];
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
+        <Portal>
+          <Modal
+            visible={visible}
+            onDismiss={() => setVisible(false)}
+            contentContainerStyle={{
+              backgroundColor: "white",
+              paddingVertical: 20,
+              paddingHorizontal: 18,
+            }}
+          >
+            <Text variant="headlineSmall" style={{ marginBottom: 14 }}>
+              新增規則
+            </Text>
+            <View
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <TextInput
+                value={ruleHr}
+                keyboardType="numeric"
+                style={{
+                  width: "25%",
+                  height: 50,
+                  padding: 10,
+                  fontSize: 20,
+                  borderStyle: "solid",
+                  borderWidth: 2,
+                  borderColor: "black",
+                  borderRadius: 12,
+                }}
+                onChangeText={(text) => setRuleHr(text)}
+              />
+              <Dropdown
+                style={{
+                  width: "25%",
+                  height: 50,
+                  padding: 10,
+                  borderStyle: "solid",
+                  borderWidth: 2,
+                  borderColor: "black",
+                  borderRadius: 12,
+                }}
+                placeholderStyle={{ fontSize: 20 }}
+                selectedTextStyle={{ fontSize: 20 }}
+                data={data1}
+                labelField="label"
+                valueField="value"
+                placeholder="選取"
+                value={type}
+                onChange={(item) => {
+                  setType(item.value);
+                }}
+              />
+              <Dropdown
+                style={{
+                  width: "25%",
+                  height: 50,
+                  padding: 10,
+                  borderStyle: "solid",
+                  borderWidth: 2,
+                  borderColor: "black",
+                  borderRadius: 12,
+                }}
+                placeholderStyle={{ fontSize: 20 }}
+                selectedTextStyle={{ fontSize: 20 }}
+                data={data2}
+                labelField="label"
+                valueField="value"
+                placeholder="選取"
+                value={calc}
+                onChange={(item) => {
+                  setCalc(item.value);
+                }}
+              />
+            </View>
+            {calc === 4 && (
+              <TextInput
+                value={"30" + " $"}
+                keyboardType="numeric"
+                style={{
+                  width: "25%",
+                  height: 50,
+                  padding: 10,
+                  fontSize: 20,
+                  marginTop: 12,
+                  borderStyle: "solid",
+                  borderWidth: 2,
+                  borderColor: "black",
+                  borderRadius: 12,
+                }}
+              />
+            )}
+            <IconButton
+              style={[styles.button, { marginTop: 18 }]}
+              icon="check"
+              iconColor={"#fff"}
+              size={25}
+              onPress={() => setVisible(false)}
+            />
+          </Modal>
+        </Portal>
         <View
           style={{
             paddingVertical: 20,
@@ -22,14 +140,18 @@ export default function Home() {
         >
           <Text variant="displayMedium">停車費用計算</Text>
         </View>
+        <Text
+          variant="headlineSmall"
+          style={{ paddingHorizontal: 20, paddingVertical: 8 }}
+        >
+          基本費用
+        </Text>
         <View
           style={{
             display: "flex",
             flexDirection: "row",
-            // backgroundColor: "lightblue",
             widthƒ: "100%",
-            padding: 20,
-            gap: 12,
+            paddingHorizontal: 20,
             alignItems: "center",
           }}
         >
@@ -44,19 +166,22 @@ export default function Home() {
               icon="menu-up-outline"
               iconColor={"#fff"}
               size={25}
-              onPress={() => console.log("Pressed")}
+              onPress={() => setRate(parseFloat(rate) + 1)}
             />
             <TextInput
-              value={"20 $"}
+              value={rate.toString()}
               keyboardType="numeric"
               style={styles.input}
+              onChangeText={(text) => setRate(text)}
             />
             <IconButton
               style={styles.button}
               icon="menu-down-outline"
               iconColor={"#fff"}
               size={25}
-              onPress={() => console.log("Pressed")}
+              onPress={() => {
+                if (rate > 1) setRate(parseFloat(rate) - 1);
+              }}
             />
           </View>
           <Text
@@ -74,19 +199,22 @@ export default function Home() {
               icon="menu-up-outline"
               iconColor={"#fff"}
               size={25}
-              onPress={() => console.log("Pressed")}
+              onPress={() => setRateHr(parseFloat(rateHr) + 1)}
             />
             <TextInput
-              value={"30"}
+              value={rateHr.toString()}
               keyboardType="numeric"
               style={styles.input}
+              onChangeText={(text) => setRateHr(text)}
             />
             <IconButton
               style={styles.button}
               icon="menu-down-outline"
               iconColor={"#fff"}
               size={25}
-              onPress={() => console.log("Pressed")}
+              onPress={() => {
+                if (rateHr > 1) setRateHr(parseFloat(rateHr) - 1);
+              }}
             />
           </View>
           <Text
@@ -96,8 +224,39 @@ export default function Home() {
               textAlign: "center",
             }}
           >
-            分鐘
+            hr
           </Text>
+        </View>
+        <View
+          style={{
+            paddingVertical: 20,
+            width: "100%",
+            // alignItems: "center",
+            paddingHorizontal: 20,
+            // backgroundColor: "green",
+          }}
+        >
+          <Text variant="headlineSmall" style={{ marginBottom: 10 }}>
+            計算規則
+          </Text>
+          <IconButton
+            style={styles.button}
+            icon="plus-circle-outline"
+            iconColor={"#fff"}
+            size={25}
+            onPress={() => setVisible(true)}
+          />
+          <IconButton
+            style={styles.button}
+            icon="alert-circle-outline"
+            iconColor={"#fff"}
+            size={25}
+            onPress={() =>
+              alert(
+                `Rate: ${rate}, RateHr: ${rateHr}, RuleHr: ${ruleHr}, Type: ${type}, Calc: ${calc}`
+              )
+            }
+          />
         </View>
         <StatusBar backgroundColor={"#258AEA"} style="auto" />
       </View>
@@ -124,8 +283,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#eee",
     borderRadius: 12,
     width: "100%",
-    padding: 20,
-    fontSize: 30,
-    marginVertical: 10,
+    padding: 12,
+    fontSize: 24,
+    // marginVertical: 10,
   },
 });
